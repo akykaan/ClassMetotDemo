6a18c80dd6a5005a5027137a4b9aae528a9d2876
kaan akyüz <kaanaky77@gmail.com>
2021-01-23 20:52:47 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+	class MusteriManager
+	{
+		List<Musteri> musteriler = new List<Musteri> { }; // müsteri bilgisini tutmak için
+
+		public void MusteriEkleme(Musteri musteri)
+		{
+			Console.WriteLine("--------Musteri Ekleme--------");
+			Console.WriteLine("{0} İsimli Musteri Eklendi!!\n",musteri.Ad);
+			musteriler.Add(musteri);
+		}
+
+		public void MusteriListele()
+		{
+			Console.WriteLine("--------Musteri Listeleme--------\n");
+			
+			foreach (var musteri in musteriler)
+			{
+				Console.WriteLine(
+					"Ad: " + musteri.Ad + " " +
+					"Soyad: " + musteri.Soyad + " " +
+					"Yas: " + musteri.Yas + " " +
+					"Id: " + musteri.Id + " " +
+					"Adres: " + musteri.Adres);
+			}
+		}
+
+		public void MusteriSilme(Musteri musteri)
+		{
+			Console.WriteLine("\n--------Musteri Silme--------\n");
+			Console.WriteLine("{0} isimli müsteri silinmiştir.\n",musteri.Ad);
+			musteriler.Remove(musteri);
+		}
+	}
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hoşgeldiniz");
+
+			Musteri musteri = new Musteri(); //ilk müşterimizi oluşturduk
+			musteri.Ad = "Furkan Kaan";
+			musteri.Soyad = "Akyüz";
+			musteri.Yas = 26;
+			musteri.Id = 1;
+			musteri.Adres = "Ankara";
+
+			Musteri musteri1 = new Musteri();
+			musteri1.Ad = "Dilara";
+			musteri1.Soyad = "Orhan";
+			musteri1.Yas = 22;
+			musteri1.Id = 2;
+			musteri1.Adres = "Ankara";
+
+
+			MusteriManager musteriManager = new MusteriManager();
+			musteriManager.MusteriEkleme(musteri); // ilk müşterimizi ekledik
+			musteriManager.MusteriEkleme(musteri1);
+
+			musteriManager.MusteriListele(); // müsteri listeleme
+
+			musteriManager.MusteriSilme(musteri); // müsteri Silme
+
+			musteriManager.MusteriListele(); // listenin son haline bakmak için çağırdık
+
+		}
+	}
+}
